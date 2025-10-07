9df5fa1e22b92473b40f8c23f2db8494b778e461
Johannes Birgmeier <jbirgmei@stanford.edu>
2025-10-07 12:34:19 +0200

    More table loading

--- processing/src/processing/types/entrez_conversion.py
+++ processing/src/processing/types/entrez_conversion.py
@@ -13,13 +13,24 @@ class EntrezConversion:
     species: Literal["human", "mouse", "zebrafish"]
     out_column_name: str
     ignore_missing: list[str]
+    to_upper: bool
+    replace: dict[str, str]
 
     def __post_init__(self):
         if self.species not in ["human", "mouse", "zebrafish"]:
             raise ValueError(f"Invalid species: {self.species}")
+        if self.to_upper not in [True, False]:
+            raise ValueError(f"Invalid to_upper: {self.to_upper}")
+        if not isinstance(self.replace, dict):  # type: ignore
+            raise ValueError(f"Invalid replace: {self.replace}")
+        for key, value in self.replace.items():
+            if not isinstance(key, str) or not isinstance(value, str):  # type: ignore
+                raise ValueError(f"Invalid replace: {self.replace}")
 
     @classmethod
     def from_json(cls, json_data: dict[str, Any]) -> "EntrezConversion":
+        to_upper = json_data["to_upper"] if "to_upper" in json_data else False
+        replace: dict[str, str] = json_data["replace"] if "replace" in json_data else {}
         return cls(
             column_name=json_data["column_name"],
             species=json_data["species"],
@@ -27,6 +38,8 @@ class EntrezConversion:
             ignore_missing=(
                 json_data["ignore_missing"] if "ignore_missing" in json_data else []
             ),
+            to_upper=to_upper,
+            replace=replace,
         )
 
     def resolve_entrez_genes(self, data: pd.DataFrame, in_path: Path) -> None:
@@ -37,6 +50,10 @@ class EntrezConversion:
         in_column_list: list[str] = data[self.column_name].tolist()
         out_data: list[str] = []
         for elem in in_column_list:
+            if self.to_upper:
+                elem = elem.upper()
+            if elem in self.replace:
+                elem = self.replace[elem]
             if elem not in entrez_gene_maps[self.species]:
                 if elem not in self.ignore_missing:
                     get_sspsygene_logger().warning(
